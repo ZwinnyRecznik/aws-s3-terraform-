92172d57d455f43dcf553e0c67e9036e568d3b58
ZwinnyRecznik <152792430+ZwinnyRecznik@users.noreply.github.com>
2024-05-26 13:23:37 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
+output "bucket_id" {
+  description = "The ID of the created S3 bucket."
+  value       = aws_s3_bucket.my_bucket.id
+}
+
+output "bucket_arn" {
+  description = "The ARN of the created S3 bucket."
+  value       = aws_s3_bucket.my_bucket.arn
+}
 
